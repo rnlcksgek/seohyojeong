3598146d452e4acd416fe00f51477ce9e41457b6
rnlcksgek <jeongyeob123@gmail.com>
2020-08-24 14:26:40 +0900

    초기

---
+++ course/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def course(request):
+
+
+
+    return render(request,'course.html')
+
+def mycourse(request):
+
+
+
+
+def course()
+
+
+
